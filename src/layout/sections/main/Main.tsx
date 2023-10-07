587ead320ccce99f2b1cbfa6950dcd7468d6a450
Anastasija <anastasijakoles@gmail.com>
2023-10-07 22:56:02 +0300

    feat: main section refacturing (add styles files)

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,254 +1,37 @@
 import React from 'react';
-/* import photo from './photo.webp'; */
 import photo from '../../../assets/images/photo.webp';
-import styled from 'styled-components';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Button } from '../../../components/Button';
 import { Container } from '../../../components/Container';
-import { Thema } from '../../../styles/Thema';
-import { font } from '../../../styles/Common';
+import { S } from './Main_Styles';
 
-export const Main = () => {
+export const Main: React.FC = () => {
   return (
-    <StyledMain>
+    <S.Main>
       <Container>
         <FlexWrapper align='center' wrap='wrap'>
-          <ContentWrapper>
+          <S.ContentWrapper>
 
-            <TextContent>
-              <StyledSpan>Hi Everyone!</StyledSpan>
-              <MainContent>
-                <Name>Anastasiya Kalesni<span>kava</span></Name>
-                <MainTitle>Frontend Developer</MainTitle>
-              </MainContent>
-              <Text>Make designs mainly logos, visual identities, apps & websites, social media and magazines.</Text>
+            <S.TextContent>
+              <S.Span>Hi Everyone!</S.Span>
+              <S.MainContent>
+                <S.Name>Anastasiya Kalesni<span>kava</span></S.Name>
+                <S.MainTitle>Frontend Developer</S.MainTitle>
+              </S.MainContent>
+              <S.Text>Make designs mainly logos, visual identities, apps & websites, social media and magazines.</S.Text>
               <Button styleType='outlined' type='button'>Get in Touch</Button>
-            </TextContent>
+            </S.TextContent>
 
-            <OuterWrapper>
-              <PhotoWrapper>
-                <StyledPhoto src={photo} alt='photo'></StyledPhoto>
-              </PhotoWrapper>
-            </OuterWrapper>
+            <S.OuterWrapper>
+              <S.PhotoWrapper>
+                <S.Photo src={photo} alt='photo'></S.Photo>
+              </S.PhotoWrapper>
+            </S.OuterWrapper>
 
-          </ContentWrapper>
+          </S.ContentWrapper>
         </FlexWrapper>
       </Container>
-    </StyledMain>
+    </S.Main>
   );
 };
-
-const StyledMain = styled.section`
-  display: flex;
-  padding-bottom: 120px;
-
-  @media ${Thema.media.tablet} {
-    padding-top: 40px;
-    padding-bottom: 80px;
-  }
-`
-
-const ContentWrapper = styled.div`
-  display: flex;
-  min-height: 445px;
-  position: relative;
-  z-index: 10;
-  width: 100%;
-
-  @media ${Thema.media.large} {
-    flex-direction: column;
-  }
-`
-
-const TextContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: space-between;
-  z-index: 10;
-
-  @media ${Thema.media.large} {
-    margin-bottom: 80px;
-    min-height: 350px;
-  }
-
-  @media ${Thema.media.mobile} {
-    margin-bottom: 55px;
-    min-height: 265px;
-  }
-
-  button {
-    font-size: 1.9rem;
-    font-weight: 600;
-    letter-spacing: 1.9px;
-    text-transform: capitalize;
-    width: 175px;
-    height: 55px;
-
-
-    @media ${Thema.media.mobile} {
-      margin: 0 auto;
-      font-size: 1.5rem;
-      width: 170px;
-      height: 50px;
-    }
-  }
-`
-
-const StyledSpan = styled.span`
-  font-size: 18px;
-
-  @media ${Thema.media.mobile} {
-    font-size: 1.4rem;
-  }
-`
-
-const MainContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-`
-
-const MainTitle = styled.h1`
-  ${font({fontFamily: `'Epilogue', sans-serif`, weight: 800, color: Thema.colors.accent})}
-  font-size: 6rem;
-
-  @media ${Thema.media.tablet} {
-    font-size: 4.5rem;
-  }
-
-  @media ${Thema.media.mobile} {
-    font-size: 2.3rem;
-  }
-`
-
-const Name = styled.h2`
-/* mixin */
-  ${font({fontFamily: `'Epilogue', sans-serif`, weight: 800})}
-  letter-spacing: -0.6px;
-  font-size: 6rem;
-
-  @media ${Thema.media.tablet} {
-    font-size: 4.5rem;
-  }
-
-  @media ${Thema.media.mobile} {
-    font-size: 2.3rem;
-  }
-
-  span {
-    color: white;
-
-    @media ${Thema.media.large} {
-      color: ${Thema.colors.text}
-    }
-
-    @media ${Thema.media.tablet} {
-      font-size: 4.5rem;
-    }
-
-    @media ${Thema.media.mobile} {
-      margin-bottom: 5px;
-      font-size: 2.3rem;
-    }
-  }
-`
-
-const Text = styled.p`
-  font-size: 2rem;
-  width: 60%;
-  line-height: 2;
-  display: inline-block;
-  text-align: left;
-
-  @media ${Thema.media.mobile} {
-    font-size: 1.4rem;
-    line-height: 1.5;
-    width: 100%;
-  }
-`
-
-const OuterWrapper = styled.div`
-  position: absolute;
-  right: 0px;
-  width: 445px;
-  height: 445px;
-
-
-  @media ${Thema.media.large} {
-    position: relative;
-    right: 0;
-    left: 50%;
-    transform: translate(-50%);
-  }
-
-  @media ${Thema.media.tablet} {
-    width: 400px;
-    height: 400px;
-  }
-
-  @media ${Thema.media.mobile} {
-    width: 250px;
-    height: 250px;
-  }
-
-  &::before {
-    content: "";
-    display: block;
-    width: 445px;
-    height: 445px;
-    background-color: ${Thema.colors.primaryBg};
-    position: absolute;
-    transform: rotate(-7deg);
-    z-index: 0;
-
-    @media ${Thema.media.tablet} {
-      width: 400px;
-      height: 400px;
-
-    }
-
-    @media ${Thema.media.mobile} {
-      width: 250px;
-      height: 250px;
-    }
-  }
-`
-
-const PhotoWrapper = styled.div`
-  position: relative;
-  z-index: 1;
-  height: 445px;
-  width: 445px;
-  transform: rotate(7.5deg);
-  overflow: hidden;
-
-  @media ${Thema.media.tablet} {
-    height: 400px;
-    width: 400px;
-  }
-
-  @media ${Thema.media.mobile} {
-    height: 250px;
-    width: 250px;
-  }
-`
-
-type StyledPhotoPropsType = {
-  src: string,
-  alt: string,
-}
-
-const StyledPhoto = styled.img<StyledPhotoPropsType>`
-  height: 100%;
-  width: 100%;
-  object-fit: cover;
-  transform: scale(1.7) translateY(70px) translateX(20px);
-
-  @media ${Thema.media.mobile} {
-    transform: scale(1.7) translateY(35px) translateX(10px);
-  }
-
-`
-
 
